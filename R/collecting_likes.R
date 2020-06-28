ecaceb6ee5250779d30a209742b83ad9d964657d
Thomas Vroylandt <tvroylandt@gmail.com>
2020-06-28 13:34:19 +0200

    GH Actions 6

--- R/collecting_likes.R
+++ R/collecting_likes.R
@@ -2,90 +2,85 @@
 # Collecting likes #
 # ----------------------- #
 
-# library(rtweet)
-# library(tidyverse)
+library(rtweet)
+library(tidyverse)
 # library(googlesheets4)
 
-Sys.getenv("TW_API_KEY")
-Sys.getenv("TW_SECRET_KEY")
-Sys.getenv("TW_ACCESS_TOKEN")
-Sys.getenv("TW_SECRET_TOKEN")
-# 
-# # Twitter Auth ------------------------------------------------------------
-# token <- create_token(
-#   app = "morphea_tv",
-#   consumer_key = Sys.getenv("TW_API_KEY"),
-#   consumer_secret = Sys.getenv("TW_SECRET_KEY"),
-#   access_token = Sys.getenv("TW_ACCESS_TOKEN"),
-#   access_secret = Sys.getenv("TW_SECRET_TOKEN")
-# )
-# 
-# # Importing likes ---------------------------------------------------------
-# df_fav <- get_favorites(user = "tvroylandt", n = 3000)
-# 
-# # Cleaning the data -------------------------------------------------------
-# # columns selection
-# df_fav_select <- df_fav %>%
-#   select(
-#     user_id,
-#     status_id,
-#     created_at,
-#     text,
-#     is_quote,
-#     quoted_text,
-#     quoted_status_id,
-#     quoted_user_id,
-#     hashtags,
-#     urls_url,
-#     media_url,
-#     favorite_count,
-#     retweet_count
-#   )
-# 
-# # infos about users
-# df_fav_users_infos <- df_fav %>%
-#   users_data() %>%
-#   select(user_id,
-#          screen_name,
-#          name,
-#          followers_count,
-#          friends_count,
-#          description,
-#          url) %>%
-#   distinct()
-# 
-# # infos about quoted users
-# df_fav_quoted_users_infos <- df_fav %>%
-#   filter(is_quote == TRUE) %>%
-#   select(quoted_status_id) %>%
-#   pull() %>%
-#   lookup_tweets() %>%
-#   users_data() %>%
-#   select(user_id, screen_name, name) %>%
-#   distinct() %>%
-#   rename_all(function(x)
-#     paste0("quoted_", x))
-# 
-# # joining all infos + cleaning list
-# df_fav_clean <- df_fav_select %>%
-#   left_join(df_fav_users_infos, by = "user_id") %>%
-#   left_join(df_fav_quoted_users_infos, by = "quoted_user_id") %>%
-#   mutate(
-#     hashtags = paste0(hashtags),
-#     hashtags = str_remove_all(hashtags, 'c\\(|\\)|"'),
-#     hashtags = str_replace_all(hashtags, ", ", " \n"),
-#     hashtags = case_when(hashtags != "NA" ~ hashtags),
-#     urls_url = paste0(urls_url),
-#     urls_url = str_remove_all(urls_url, 'c\\(|\\)|"'),
-#     urls_url = str_replace_all(urls_url, ", ", " \n"),
-#     urls_url = case_when(urls_url != "NA" ~ urls_url)
-#   ) %>%
-#   unnest(cols = c(media_url)) %>%
-#   group_by(status_id) %>%
-#   mutate(n = row_number()) %>%
-#   pivot_wider(values_from = c(media_url), names_from = n) %>%
-#   ungroup()
-# 
+# Twitter Auth ------------------------------------------------------------
+token <- create_token(
+  app = "morphea_tv",
+  consumer_key = Sys.getenv("TW_API_KEY"),
+  consumer_secret = Sys.getenv("TW_SECRET_KEY"),
+  access_token = Sys.getenv("TW_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TW_SECRET_TOKEN")
+)
+
+# Importing likes ---------------------------------------------------------
+df_fav <- get_favorites(user = "tvroylandt", n = 3000)
+
+# Cleaning the data -------------------------------------------------------
+# columns selection
+df_fav_select <- df_fav %>%
+  select(
+    user_id,
+    status_id,
+    created_at,
+    text,
+    is_quote,
+    quoted_text,
+    quoted_status_id,
+    quoted_user_id,
+    hashtags,
+    urls_url,
+    media_url,
+    favorite_count,
+    retweet_count
+  )
+
+# infos about users
+df_fav_users_infos <- df_fav %>%
+  users_data() %>%
+  select(user_id,
+         screen_name,
+         name,
+         followers_count,
+         friends_count,
+         description,
+         url) %>%
+  distinct()
+
+# infos about quoted users
+df_fav_quoted_users_infos <- df_fav %>%
+  filter(is_quote == TRUE) %>%
+  select(quoted_status_id) %>%
+  pull() %>%
+  lookup_tweets() %>%
+  users_data() %>%
+  select(user_id, screen_name, name) %>%
+  distinct() %>%
+  rename_all(function(x)
+    paste0("quoted_", x))
+
+# joining all infos + cleaning list
+df_fav_clean <- df_fav_select %>%
+  left_join(df_fav_users_infos, by = "user_id") %>%
+  left_join(df_fav_quoted_users_infos, by = "quoted_user_id") %>%
+  mutate(
+    hashtags = paste0(hashtags),
+    hashtags = str_remove_all(hashtags, 'c\\(|\\)|"'),
+    hashtags = str_replace_all(hashtags, ", ", " \n"),
+    hashtags = case_when(hashtags != "NA" ~ hashtags),
+    urls_url = paste0(urls_url),
+    urls_url = str_remove_all(urls_url, 'c\\(|\\)|"'),
+    urls_url = str_replace_all(urls_url, ", ", " \n"),
+    urls_url = case_when(urls_url != "NA" ~ urls_url)
+  ) %>%
+  unnest(cols = c(media_url)) %>%
+  group_by(status_id) %>%
+  mutate(n = row_number()) %>%
+  pivot_wider(values_from = c(media_url), names_from = n) %>%
+  ungroup()
+
 # # Google Auth -------------------------------------------------------------
 # # Sheet is public with the link here
 # gs4_deauth()
