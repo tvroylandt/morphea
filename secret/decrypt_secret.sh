c767d45c7f94b7091e144efa7406c76417619877
Thomas Vroylandt <tvroylandt@gmail.com>
2020-06-28 16:38:57 +0200

    Secret encryption 2

--- secret/decrypt_secret.sh
+++ secret/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secret
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secret/morphea_token.json morphea_token.gpg
+--output $HOME/secret/morphea_token.json $HOME/secret/morphea_token.json.gpg
