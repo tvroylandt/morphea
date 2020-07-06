6dea69d039cf11b440d1f6ecb94aad4a908bd618
Thomas Vroylandt <tvroylandt@gmail.com>
2020-07-06 22:24:51 +0200

    Style ok

--- R/collecting_likes.R
+++ R/collecting_likes.R
@@ -31,8 +31,8 @@ df_fav_select <- df_fav %>%
     quoted_status_id,
     quoted_user_id,
     hashtags,
-    urls_url,
-    media_url,
+    urls_expanded_url,
+    ext_media_url,
     favorite_count,
     retweet_count
   )
@@ -50,60 +50,69 @@ df_fav_users_infos <- df_fav %>%
   distinct()
 
 # infos about quoted users
-df_fav_quoted_users_infos <- df_fav %>%
+df_quoted_tweets <- df_fav %>%
   filter(is_quote == TRUE) %>%
   select(quoted_status_id) %>%
   pull() %>%
-  lookup_tweets() %>%
+  lookup_tweets() 
+
+df_fav_quoted_users_infos <- df_quoted_tweets %>%
   users_data() %>%
   select(user_id, screen_name, name) %>%
   distinct() %>%
   rename_all(function(x)
     paste0("quoted_", x))
 
+df_quoted_media <- df_quoted_tweets %>%
+  select(status_id, ext_media_url,
+         urls_expanded_url) %>%
+  rename(quoted_ext_media_url = ext_media_url,
+         quoted_urls_expanded_url = urls_expanded_url)
+
 # joining all infos + cleaning lists
 df_fav_clean <- df_fav_select %>%
   left_join(df_fav_users_infos, by = "user_id") %>%
   left_join(df_fav_quoted_users_infos, by = "quoted_user_id") %>%
+  left_join(df_quoted_media, by = c("quoted_status_id" = "status_id")) %>%
+  rtweet::flatten() %>%
   mutate(
-    hashtags = paste0(hashtags),
-    hashtags = str_remove_all(hashtags, 'c\\(|\\)|"'),
-    hashtags = str_replace_all(hashtags, ", ", " \n"),
-    hashtags = case_when(hashtags != "NA" ~ hashtags),
-    urls_url = paste0(urls_url),
-    urls_url = str_remove_all(urls_url, 'c\\(|\\)|"'),
-    urls_url = str_replace_all(urls_url, ", ", " \n"),
-    urls_url = case_when(urls_url != "NA" ~ urls_url)
+    link_tw = paste0("https://twitter.com/", screen_name, "/status/", status_id),
+    quoted_link_tw = paste0(
+      "https://twitter.com/",
+      quoted_screen_name,
+      "/status/",
+      quoted_status_id
+    ),
+    urls_expanded_url = str_remove(urls_expanded_url, quoted_link_tw),
+    urls_expanded_url = str_trim(urls_expanded_url, "both")
   ) %>% 
-  unnest(c(media_url)) # only return the first image ???
-
-# use https://docs.ropensci.org/rtweet/reference/flatten.html for list-columns ?
+  arrange(desc(created_at))
 
 # Google Auth -------------------------------------------------------------
 # need to allow the API for the sheet
 gs4_auth(email = Sys.getenv("GOOGLE_MAIL"),
          path = "secret/morphea_token.json")
 
-# Importing Google Sheets -------------------------------------------------
-df_tw_sheet <-
-  read_sheet(Sys.getenv("SHEET_PATH"),
-             sheet = "tw_fav")
-
-# Merging data ------------------------------------------------------------
-df_fav_tw_supp <- df_fav_clean %>%
-  filter(!status_id %in% unique(df_tw_sheet$status_id))
-
-df_tw_sheet_full <- df_tw_sheet %>%
-  bind_rows(df_fav_tw_supp) %>% 
-  arrange(desc(created_at))
+# # Importing Google Sheets -------------------------------------------------
+# df_tw_sheet <-
+#   read_sheet(Sys.getenv("SHEET_PATH"),
+#              sheet = "tw_fav")
+# 
+# # Merging data ------------------------------------------------------------
+# df_fav_tw_supp <- df_fav_clean %>%
+#   filter(!status_id %in% unique(df_tw_sheet$status_id))
+# 
+# df_tw_sheet_full <- df_tw_sheet %>%
+#   bind_rows(df_fav_tw_supp) %>% 
+#   arrange(desc(created_at))
 
 # Categorising data -------------------------------------------------------
 
 # Intermediate save -------------------------------------------------------
 # Saved as artifact
-write_csv(df_tw_sheet_full, "tw_fav.csv")
+write_csv(df_fav_clean, "tw_fav.csv")
 
 # Exporting to Google Sheets ----------------------------------------------
-write_sheet(df_tw_sheet_full,
+write_sheet(df_fav_clean,
             ss = Sys.getenv("SHEET_PATH"),
             sheet = "tw_fav")
